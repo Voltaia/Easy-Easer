2732c609cebee1f2e8b57c76a06e058871ff8bc2
Harper Rhett <harperrhett@gmail.com>
2024-08-14 15:37:33 -0500

    Added example

---
+++ Example.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using EasyEaser;
+
+public class Example : MonoBehaviour
+{
+	// Inspector
+	public Vector3 DesiredPosition;
+	public Color DesiredColor;
+	public float EaseSeconds;
+
+	// General
+	private SpriteRenderer spriteRenderer;
+
+	private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		StartCoroutine(EaseSelf());
+	}
+
+	private IEnumerator EaseSelf()
+	{
+		// Get starting state
+		Vector3 startPosition = transform.position;
+		Color startColor = spriteRenderer.color;
+
+		// Create an easer that lasts for some amount of seconds with a smooth curve
+		Easer easer = new(EaseSeconds);
+		easer.CurveType = Easer.Curve.Smooth;
+
+		// Ease until easer is finished
+		while (easer.IsEasing)
+		{
+			transform.position = easer.EaseVector(startPosition, DesiredPosition);
+			spriteRenderer.color = easer.EaseColor(startColor, DesiredColor);
+			yield return null;
+		}
+	}
+}
